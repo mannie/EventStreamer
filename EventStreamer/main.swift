52f48d42bf0ec23ea4cf48bc70a0682f8086ab1c
Mannie <mail@mannie.io>
2018-05-21 01:31:40 -0400

    Initial Commit

---
+++ EventStreamer/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  EventStreamer
+//
+//  Created by Mannie Tagarira on 5/21/18.
+//  Copyright © 2018 Microsoft. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
